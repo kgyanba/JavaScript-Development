a8fabb9000c1fb74c3cc7a4d9c5d8c26ef407e49
kgyanba <kgyanba@gmail.com>
2018-08-06 20:26:54 +0530

    added

---
+++ data-type/script.js
@@ -0,0 +1,18 @@
+// String example
+
+var abc = "Hello";
+console.log("string is " + abc + " data type is :" +typeof abc)
+
+// number example
+
+var abc = 10;
+console.log("the number is " + abc + " data type is :" +typeof abc)
+
+// boolean
+
+var abc = true;
+console.log(" the boolean is " + abc + " data type is :" +typeof abc)
+
+// undefined example
+var xyz;
+console.log(" value :" + xyz + " data type :" +typeof xyz)
